0837f7ee92f4a6edb2cec7e0d10ad06029ab10f4
gogo4ds <gogo4ds@gmail.com>
2022-03-02 16:05:09 +0200

    Added form in Index.cshtml

--- src/AutoCrudAdmin/ViewModels/AutoCrudAdminGridToolbarActionViewModel.cs
+++ src/AutoCrudAdmin/ViewModels/AutoCrudAdminGridToolbarActionViewModel.cs
@@ -1,9 +1,13 @@
 namespace AutoCrudAdmin.ViewModels;
 
 using System.Collections.Generic;
+using System.Linq;
 
 public class AutoCrudAdminGridToolbarActionViewModel : GridAction
 {
     public IDictionary<string, string> RouteValues { get; set; }
         = new Dictionary<string, string>();
+
+    public IEnumerable<FormControlViewModel> FormControls { get; set; }
+        = Enumerable.Empty<FormControlViewModel>();
 }
