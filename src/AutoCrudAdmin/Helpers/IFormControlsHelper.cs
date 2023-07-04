3a4c2e3407acbc11ddf56a1c0df1665d47ad5c3b
msmner <ivaylo.nikolov88@gmail.com>
2023-07-04 17:26:59 +0300

    add documentation and fix a bug with autocomplete

--- src/AutoCrudAdmin/Helpers/IFormControlsHelper.cs
+++ src/AutoCrudAdmin/Helpers/IFormControlsHelper.cs
@@ -7,6 +7,15 @@ namespace AutoCrudAdmin.Helpers
 
     public interface IFormControlsHelper
     {
+        /// <summary>
+        /// Generates the form controls for the respective entity.
+        /// </summary>
+        /// <param name="entity">The entity we want to generate the form controls for.</param>
+        /// <param name="entityAction">Create, edit or delete form control action.</param>
+        /// <param name="complexOptionFilters">Filters for generating complex form controls.</param>
+        /// <param name="autocompleteType">The type of the autocomplete form control.</param>
+        /// <typeparam name="TEntity">The entity we are generating the form controls for.</typeparam>
+        /// <returns>The generated form controls.</returns>
         IEnumerable<FormControlViewModel> GenerateFormControls<TEntity>(
             TEntity entity,
             EntityAction entityAction,
