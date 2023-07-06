1b3646e191637ad666922f86f13bbe39f3326402
msmner <ivaylo.nikolov88@gmail.com>
2023-07-06 12:51:36 +0300

    Update documentation

--- src/AutoCrudAdmin/Helpers/IFormControlsHelper.cs
+++ src/AutoCrudAdmin/Helpers/IFormControlsHelper.cs
@@ -13,7 +13,8 @@ namespace AutoCrudAdmin.Helpers
         /// <param name="entity">The entity we want to generate the form controls for.</param>
         /// <param name="entityAction">The action to be performed on the entity.</param>
         /// <param name="complexOptionFilters">Optional. A dictionary containing complex option filters, based on which we are loading the data for the form control.</param>
-        /// <param name="autocompleteType">Optional. The type used for autocomplete functionality.</param>
+        /// <param name="autocompleteType">Optional. The type of the property that will be used for autocomplete functionality.
+        /// When passed, values for this property will not be loaded immediately, but when searched for.</param>
         /// <typeparam name="TEntity">The type of the entity.</typeparam>
         /// <returns>An enumerable collection of FormControlViewModel objects.</returns>
         IEnumerable<FormControlViewModel> GenerateFormControls<TEntity>(
