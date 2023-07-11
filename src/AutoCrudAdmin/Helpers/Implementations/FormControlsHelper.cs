9f38fdba79100947ce8fb2d6728befbc496a67e1
Doncho Minkov <donchominkov@gmail.com>
2023-07-11 15:15:44 +0300

    Adding some docs

--- src/AutoCrudAdmin/Helpers/Implementations/FormControlsHelper.cs
+++ src/AutoCrudAdmin/Helpers/Implementations/FormControlsHelper.cs
@@ -48,15 +48,51 @@ namespace AutoCrudAdmin.Helpers.Implementations
 
         private static ISet<Type> Types { get; set; }
 
+        /// <summary>
+        /// Generates the form controls for the respective entity.
+        /// </summary>
+        /// <param name="entity">The entity we want to generate the form controls for.</param>
+        /// <param name="entityAction">The action to be performed on the entity.</param>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <returns>An enumerable collection of FormControlViewModel objects.</returns>
+        public IEnumerable<FormControlViewModel> GenerateFormControls<TEntity>(TEntity entity, EntityAction entityAction)
+            => this.GenerateFormControls(entityAction, entityAction, null, null);
+
+        /// <summary>
+        /// Generates the form controls for the respective entity.
+        /// </summary>
+        /// <param name="entity">The entity we want to generate the form controls for.</param>
+        /// <param name="entityAction">The action to be performed on the entity.</param>
+        /// <param name="complexOptionFilters">Optional. A dictionary containing complex option filters, based on which we are loading the data for the form control.</param>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <returns>An enumerable collection of FormControlViewModel objects.</returns>
+        public IEnumerable<FormControlViewModel> GenerateFormControls<TEntity>(TEntity entity, EntityAction entityAction, IDictionary<string, Expression<Func<object, bool>>> complexOptionFilters)
+            => this.GenerateFormControls(entityAction, entityAction, complexOptionFilters, null);
+
+        /// <summary>
+        /// Generates the form controls for the respective entity.
+        /// </summary>
+        /// <param name="entity">The entity we want to generate the form controls for.</param>
+        /// <param name="entityAction">The action to be performed on the entity.</param>
+        /// <param name="complexOptionFilters">Optional. A dictionary containing complex option filters, based on which we are loading the data for the form control.</param>
+        /// <param name="autocompleteType">Optional. The type of the property that will be used for autocomplete functionality.
+        /// When passed, values for this property will not be loaded immediately, but when searched for.</param>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <returns>An enumerable collection of FormControlViewModel objects.</returns>
         public IEnumerable<FormControlViewModel> GenerateFormControls<TEntity>(
             TEntity entity,
             EntityAction entityAction,
-            IDictionary<string, Expression<Func<object, bool>>>? complexOptionFilters = null,
-            Type? autocompleteType = null)
+            IDictionary<string, Expression<Func<object, bool>>>? complexOptionFilters,
+            Type autocompleteType)
             => this.GeneratePrimaryKeyFormControls(entity, entityAction, autocompleteType)
                 .Concat(GeneratePrimitiveFormControls(entity))
                 .Concat(this.GenerateComplexFormControls(entity, entityAction, complexOptionFilters));
 
+        /// <summary>
+        /// Gets complex form control name for the provided entity name.
+        /// </summary>
+        /// <param name="entityName">The entity name.</param>
+        /// <returns>The name of the complex form control.</returns>
         public string GetComplexFormControlNameForEntityName(string entityName)
             => entityName + "Id";
 
