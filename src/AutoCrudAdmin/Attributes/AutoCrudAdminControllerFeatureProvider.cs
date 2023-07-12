95a9eb7b910dd48e5f9416ee54388ec28b9fb9f2
Doncho Minkov <donchominkov@gmail.com>
2023-07-12 14:58:13 +0300

    Added docs for

--- src/AutoCrudAdmin/Attributes/AutoCrudAdminControllerFeatureProvider.cs
+++ src/AutoCrudAdmin/Attributes/AutoCrudAdminControllerFeatureProvider.cs
@@ -10,7 +10,10 @@ namespace AutoCrudAdmin.Attributes
     using Microsoft.AspNetCore.Mvc.ApplicationParts;
     using Microsoft.AspNetCore.Mvc.Controllers;
 
-    public class AutoCrudAdminControllerFeatureProvider : IApplicationFeatureProvider<ControllerFeature>
+    /// <summary>
+    /// <see cref="AutoCrudAdminControllerFeatureProvider"/> is an internal class that acts as a feature provider for AutoCrudAdmin controllers.
+    /// </summary>
+    internal class AutoCrudAdminControllerFeatureProvider : IApplicationFeatureProvider<ControllerFeature>
     {
         static AutoCrudAdminControllerFeatureProvider()
             => Types = ReflectionHelper.DbSetProperties
@@ -22,6 +25,12 @@ namespace AutoCrudAdmin.Attributes
 
         private static IEnumerable<Type> Types { get; set; }
 
+        /// <summary>
+        /// PopulateFeature is a method that populates the feature with the controller types.
+        /// This method gets all the controller types that are part of the AutoCrudAdmin and adds them to the feature's controllers list.
+        /// </summary>
+        /// <param name="parts">The list of application parts.</param>
+        /// <param name="feature">The ControllerFeature that will be populated.</param>
         public void PopulateFeature(IEnumerable<ApplicationPart> parts, ControllerFeature feature)
             => Types
                 .ToList()
