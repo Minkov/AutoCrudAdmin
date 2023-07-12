0f2f03d4d482dde05358c8579013921b6ce133db
Doncho Minkov <donchominkov@gmail.com>
2023-07-12 15:04:26 +0300

    Added docs for

--- src/AutoCrudAdmin/Attributes/AutoCrudAdminControllerNameConvention.cs
+++ src/AutoCrudAdmin/Attributes/AutoCrudAdminControllerNameConvention.cs
@@ -8,6 +8,11 @@ namespace AutoCrudAdmin.Attributes
     using AutoCrudAdmin.Helpers;
     using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
+    /// <summary>
+    /// The <see cref="AutoCrudAdminControllerNameConvention"/> class defines a naming convention for AutoCrudAdmin controllers.
+    /// It extends the Attribute class and implements the IControllerModelConvention interface.
+    /// It's primary function is to set a controller name and route for every not explicitly named AutoCrudAdmin controller.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class AutoCrudAdminControllerNameConvention : Attribute, IControllerModelConvention
     {
@@ -29,6 +34,12 @@ namespace AutoCrudAdmin.Attributes
 
         private static Dictionary<Type, string> EntityTypeToNameMap { get; set; }
 
+        /// <summary>
+        /// The <see cref="Apply"/>  method is used to apply the naming convention to the ControllerModel.
+        /// It checks if a controller is an <see cref="AutoCrudAdminController"/> and whther is has been explicitly named.
+        /// If not explicitly named, it sets the controller name and route to the name of the entity type in plural.
+        /// </summary>
+        /// <param name="controller">The <see cref="ControllerModel"/> that the convention will be applied to.</param>
         public void Apply(ControllerModel controller)
         {
             if ((!controller.ControllerType.IsGenericType
